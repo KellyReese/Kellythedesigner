b43cdd31d2b54f81fc7382f995c26b2f5179427d
KellyReese <blkmamb@hotmail.com>
2025-04-20 11:44:22 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$(".aboutmain span,p").slideDown( 2000);
+
+});
+
+$(document).ready(function(){
+    
+    $(".featured").fadeIn(2000);
+});
+    
+
+    function showSidebar(){
+        const sidebar = document.querySelector(".sidebar")
+        sidebar.style.display = "flex"
+    }
+    function hideSidebar(){
+        const sidebar = document.querySelector(".sidebar")
+        sidebar.style.display = "none"
+    }
+
