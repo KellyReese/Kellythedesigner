adc39292b52e98354cf7567b418ae9bf253c039e
KellyReese <blkmamb@hotmail.com>
2025-05-08 16:01:57 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your phone number is: <?php echo $_POST["mobile"]; ?>
+
+</body>
+</html> 
